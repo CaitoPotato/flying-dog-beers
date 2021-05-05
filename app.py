f94e653646ace19d05bd7bca0432e01758959785
CaitoPotato <81201991+CaitoPotato@users.noreply.github.com>
2021-05-05 22:00:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 ########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+hydrogen=['Electrolysis', 'Coal Gas', 'SMR', 'Pyrolysis']
 ibu_values=[35, 60, 85, 75]
 abv_values=[5.4, 7.1, 9.2, 4.3]
 color1='darkred'
 color2='orange'
-mytitle='Beer Comparison'
-tabtitle='beer!'
-myheading='Flying Dog Beers'
+mytitle='Hydrogen Comparison'
+tabtitle='H2SC'
+myheading='Hydrogen Supply Chain'
 label1='IBU'
 label2='ABV'
 githublink='https://github.com/austinlasseter/flying-dog-beers'
